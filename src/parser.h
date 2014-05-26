243d79ebd11676bcd55287247ff1cf3ff0710fa2
Justin DeCell <decelj@jdMPB.local>
2014-05-26 11:10:53 -0700

    Removed old parser files

--- src/parser.h
+++
@@ -1,8 +0,0 @@
-#include <sstream>
-#include <string>
-
-#include "scene.h"
-
-bool readValues(std::string& line, std::stringstream &s, const int num, float* values);
-void readFile(const char* file, std::string &outputImage);
-
